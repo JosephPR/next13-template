f739330254859f3b389be1866ca72185d2ce4d87
Joseph Reilly <josephreilly@Josephs-MacBook-Air-2.local>
2023-01-04 14:51:06 -0700

    popup video

--- components/popup.jsx
+++ components/popup.jsx
@@ -8,7 +8,7 @@ export default function buttons() {
     <a
           className={styles.imageContainer}
           data-fancybox
-          href="https://images.unsplash.com/photo-1494376877685-d3d2559d4f82?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          href="https://www.youtube.com/embed/Vq8HeDX3KYY"
         >
           <img
             className={styles.imagePopup}
